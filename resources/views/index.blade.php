7a9c61e097892d000bc127f41832c7db9131ffe4
JhonattanMA <jhonatanmariiaca16@gmail.com>
2019-10-13 10:39:29 -0500

    Ultimos retoques

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>Help Me</title>
+  <title>Helpme</title>
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <meta content="" name="keywords">
   <meta content="" name="description">
@@ -26,83 +26,60 @@
 
   <!-- Main Stylesheet File -->
   <link href="css/style.css" rel="stylesheet">
-
-  <!-- =======================================================
-    Theme Name: Rapid
-    Theme URL: https://bootstrapmade.com/rapid-multipurpose-bootstrap-business-template/
-    Author: BootstrapMade.com
-    License: https://bootstrapmade.com/license/
-  ======================================================= -->
 </head>
 
 <body>
-  <!--==========================
-  Header
-  ============================-->
   <header id="header">
-
     <div class="container">
-
       <div class="float-left">
           <a href="#intro" class="scrollto">
             <img src="img/logo_app.png" alt="" width="100px" class="mb-6">
         </a>
       </div>
-
       <nav class="main-nav float-right d-none d-lg-block">
         <ul>
           <li class="active"><a href="#intro">Inicio</a></li>
-          <li class="drop-down"><a href="#about">¿Qué es Help Me?</a>
+          <li class="drop-down"><a href="#about">¿Qué es Helpme?</a>
             <ul>
               <li><a href="#why-us">Como Funciona</a></li>
               <li><a href="#call-to-action">Descargar</a></li>
             </ul>
           </li>
-          <li><a href="#services">¿Quienes Somos?</a></li>
-          <li><a href="#footer">Contactanos</a></li>
+          <li><a href="#services">¿Quiénes Somos?</a></li>
+          <li><a href="#footer">Contáctanos</a></li>
         </ul>
-      </nav><!-- .main-nav -->
-
+      </nav>
     </div>
-  </header><!-- #header -->
+  </header>
 
-  <!--==========================
-    Intro Section
-  ============================-->
   <section id="intro" class="clearfix">
     <div class="container d-flex h-100">
       <div class="row justify-content-center align-self-center">
         <div class="col-md-6 intro-info order-md-first order-last">
           <img src="img/logo_app.png" alt="" width="80%">
-          <h2><span class="span">Donando</span> a los que mas lo <span class="span">Necesitan</span></h2>
+          <h2 align="center"><span class="span">Ayuda</span> a los que más lo <span class="span">Necesitan</span></h2>
         </div>
-
         <div class="col-md-6 intro-img order-md-last order-first">
           <img src="img/donations.png" alt="Donations" class="img-fluid mt-5 ml-5" width="80%">
         </div>
       </div>
 
     </div>
-  </section><!-- #intro -->
+  </section>
 
   <main id="main">
-
-    <!--==========================
-      About Us Section
-    ============================-->
     <section id="about">
-
       <div class="container">
         <div class="row">
           <div class="col-lg-10 col-md-6 mx-auto">
             <div class="about-content">
-              <h2 align="center">¿Qué es Help Me?</h2>
-              <h3>Donando a los que mas lo necesita</h3>
-              <p>Help Me es una aplicación movil que hacerca a las persona que estan en estado de abandono y viviendo en las calles, al resto del mundo.</p>
-              <p>Con esta aplicacion se puede transmir y da a conocer las condiciones en las que se encuentran las personas en condición de abandono, por medio de fotos y comentarios dejados por las persona que tiene esta aplicación en sus dispositivos.</p>
+              <h2 align="center">¿Qué es Helpme?</h2>
+              <p>Helpme es una aplicación móvil que acerca a las persona que estan en estado de abandono y viviendo en las calles, al resto del mundo.</p>
+              <p>Con esta aplicación se puede transmitir y dar a conocer las condiciones en las que se encuentran las personas en condición de abandono por medio de fotos, comentarios y una breve historia de ellos, 
+                estos datos fueron dejados por las personas que tienen la aplicación en sus dispositivos y quiere ayudar a dichas personas en estado de abandono.</p>
               <ul>
-                <li><i class="ion-android-checkmark-circle"></i>Dar a conocer el estado de las personas en abandono.</li>
-                <li><i class="ion-android-checkmark-circle"></i>Ayudar a las personas en estado de abandono.</li>
+                <li><i class="ion-android-checkmark-circle"></i>Dar a conocer el estado de las personas en estado de abandono.</li>
+                <li><i class="ion-android-checkmark-circle"></i>Ayudar a mejorar la calidad de vida de las personas en estado de abandono.</li>
                 <li><i class="ion-android-checkmark-circle"></i>Mejoramiento de los espacios de los ciudadanos.</li>
               </ul>
             </div>
@@ -110,21 +87,16 @@
         </div>
       </div>
 
-    </section><!-- #about -->
+    </section>
 
-    <!--==========================
-      Why Us Section
-    ============================-->
     <section id="why-us" class="wow fadeIn section-bg">
       <div class="container-fluid">
 
         <header class="section-header">
-          <h3>Como Funciona Helpme</h3>
-          <p>Comó funciona Help Me?</p>
+          <h3>Comó funciona Helpme?</h3>
         </header>
 
         <div class="row">
-
           <div class="col-lg-6">
             <div class="why-us-img">
               <img src="img/funcionamiento.png" alt="" class="img-fluid mt-5 ml-5" width="70%">
@@ -133,58 +105,41 @@
 
           <div class="col-lg-6">
             <div class="why-us-content">
-              <p>Help Me funciona en dispocitivos con sistemas operativos Android. Esta aplicación sirve para compartir fotos ,comentarios y llamar a las entidades correspondientes, y asi poder dar ayuda a las personas en estado de abandono que se encuenran en la calles.</p>
               <p>
-                En Help Me te podras registrar, logiarte y asi podras subir las fotos de las personas en estado de abandono. Podras comunicarte con las entidades correspondientes. Tambien podras comentar en las fotos que otros usuarios hayan subido para dar mas apoyo a esas personas desamparadas.
+                Helpme funciona en dispositivos con sistema operativo android. En Helpme te podras registrar, logearte y asi podras subir las fotos de las personas en estado de abandono, podras comunicarte con las entidades correspondientes, escribir una breve historia del habitante o escribir en que situación se encuentra. 
+                También podras comentar en las fotos que otros usuarios hayan subido para dar más apoyo a esas personas desamparadas.
               </p>
 
               <div class="features wow bounceInUp clearfix">
                 <i class="fa fa-camera" style="color: #f058dc;"></i>
-                <h4>Sube Fotos</h4>
+                <h4>Publica Fotos</h4>
                 <p>Sube fotos y comparte como estan las personas en estado de abandono.</p>
               </div>
 
               <div class="features wow bounceInUp clearfix">
                 <i class="fa fa-book" style="color: #ffb774;"></i>
                 <h4>Comenta</h4>
-                <p>Comenta que te parece la situacion de esta persona en estado de abandono.</p>
+                <p>Comenta en que estado se encuetra la persona en estado de abandono y que te parece la situación de esta persona en estado de abandono.</p>
               </div>
 
               <div class="features wow bounceInUp clearfix">
                 <i class="fa fa-handshake-o" style="color: #589af1;"></i>
                 <h4>Ayuda</h4>
-                <p>Ayuda a la persona llamando a una entidad para darle una oportunidad de empezar de nuevo.</p>
+                <p>Ayuda a la persona llamando a una entidad para darle una oportunidad de empezar de nuevo o ayudarlo con tus donaciones.</p>
               </div>
-
             </div>
-
           </div>
-
         </div>
-
       </div>
     </section>
 
-    <!--==========================
-      Portfolio Section
-    ============================-->
     <section id="portfolio">
       <div class="container">
-
         <header class="section-header">
-          <h3 class="section-title">Fotos</h3>
+          <h3 class="section-title">Prototipo</h3>
         </header>
 
-        <div class="row">
-          <div class="col-lg-12">
-            <ul id="portfolio-flters">
-              <li data-filter="*" class="filter-active" hidden>All</li>
-            </ul>
-          </div>
-        </div>
-
         <div class="row portfolio-container">
-
           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
             <div class="portfolio-wrap">
               <img src="img/App/foto.png" class="img-fluid" alt="">
@@ -251,19 +206,15 @@
             </div>
           </div>
         </div>
-
       </div>
-    </section><!-- #portfolio -->
+    </section>
 
-    <!--==========================
-      Call To Action Section
-    ============================-->
     <section id="call-to-action" class="wow fadeInUp">
       <div class="container">
         <div class="row">
           <div class="col-lg-9 text-center text-lg-left">
             <h3 class="cta-title">Descarga Nuestra App</h3>
-            <p class="cta-text">Aqui podras encontrar nuestra aplicacion para que la instales en tu dispocitivo movil.</p>
+            <p class="cta-text">Aquí podras encontrar nuestra aplicación para que la instales en tu dispositivo móvil.</p>
           </div>
           <div class="col-lg-3 cta-btn-container text-center">
             <a class="cta-btn align-middle" href="img/favicon.png" download="Help_Me.apk">
@@ -271,19 +222,13 @@
             </a>
           </div>
         </div>
-
       </div>
-    </section><!-- #call-to-action -->
+    </section>
 
-    <!--==========================
-      Services Section
-    ============================-->
     <section id="services" class="section-bg">
       <div class="container">
-
         <header class="section-header">
-          <h3>¿Quienes Somos?</h3>
-          <p>Nosotros Somos Hack Musketeers</p>
+          <h3>¿Quiénes Somos?</h3>
         </header>
 
         <div class="row feature-item mt-5 pt-5">
@@ -292,36 +237,25 @@
           </div>
 
           <div class="col-lg-6 wow fadeInUp pt-4 pt-lg-0 order-2 order-lg-1">
-            <h4>Hack Musketeers.</h4>
+            <h4>Hackmons.</h4>
             <p>
-             Hack Musketeers es un grupo de programadores los cuales fueron que inpulsaron la idea de Help Me, con ayuda del Clouster Creatic y Simwood.
+              Hackmons es un grupo de programadores los cuales desarrollarón la idea de Helpme, con ayuda del Clouster Creatic, Telesign y Simwood.
             </p>
             <p>
-
-            </p>
-            <p>
-              Este grupo tan particular esta compuesto de 5 personas que son David Argote (Disenador,Programador), Jhonatan Mariaca (Diseñador), Daniel Mera(Programador), Edwin Hidalgo(Programador), Cristian Cañar(Marketing) que juntos haces un gran equipo de desarrollo de aplicaciones.
+              Este grupo tan particular esta compuesto de 5 personas las cuales son: Cristhian Cañar(Programador, Marketing), Jhonatan Mariaca (Diseñador, Programador), David Argote (Diseñador, Programador), Daniel Mera(Programador), Edwin Hidalgo(Programador) que juntos somos un gran equipo de desarrollo de aplicaciones móviles y web.
             </p>
           </div>
-
         </div>
-
       </div>
-    </section><!-- #services -->
+    </section>
 
-
-    <!--==========================
-      Team Section
-    ============================-->
     <section id="team">
       <div class="container">
         <div class="section-header">
           <h3>Perfiles</h3>
-          <p>Integrantes de Help Me</p>
+          <p>Integrantes de Helpme</p>
         </div>
-
         <div class="row">
-
           <div class="col-lg-4 col-md-6 wow fadeInUp">
             <div class="member">
               <img src="img/Desarrolladores/Cristhian.jpeg" class="img-fluid" alt="">
@@ -380,18 +314,10 @@
         </div>
 
       </div>
-    </section><!-- #team -->
+    </section>
 
-    <!--==========================
-      Clients Section
-    ============================-->
     <section id="clients" class="wow fadeInUp section-bg">
       <div class="container">
-
-        <header class="section-header">
-
-        </header>
-
         <div class="owl-carousel clients-carousel">
           <a href="https://www.telesign.com/" target="_blank"><img src="img/clients/telesign.png" alt=""></a>
           <a href="https://developers.apidaze.io/" target="_blank"><img src="img/clients/apidaze.png" alt=""></a>
@@ -399,46 +325,26 @@
           <a href="https://simwood.com/" target="_blank"><img src="img/clients/simwood.png" alt=""></a>
           <a href="http://clustercreatic.com/es/" target="_blank"><img src="img/clients/creatic.png" alt=""></a>
         </div>
-
       </div>
-    </section><!-- #clients -->
-
+    </section>
   </main>
 
-  <!--==========================
-    Footer
-  ============================-->
   <footer id="footer">
-    <div class="container">
-      <div class="footer-links" align="center">
-        <h4>Contactanos</h4>
-        <p>
-          Calle 4 # 2-80 <br>
-          Popayán<br>
-          Colombia, Cauca<br>
-          <strong>Celular:</strong> +57 3058172745<br>
-          <strong>Correo:</strong> daalean4@misena.edu.co<br>
-        </p>
-      </div>
+    <div class="container">     
       <div class="copyright">
-        &copy; Copyright <strong>Help Me</strong>. All Rights Reserved
-      </div>
-      <div class="credits">
-        <!--
-          All the links in the footer should remain intact.
-          You can delete the links only if you purchased the pro version.
-          Licensing information: https://bootstrapmade.com/license/
-          Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Rapid
-        -->
-        <!-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> -->
+          <h4 class="mt-5"><strong>Información de contacto:</strong></h4>
+          <p>
+              <strong>Dirección:</strong> Calle 4 # 2-80 <br>
+              <strong>Ciudad:</strong> Popayán - Cauca<br>
+            <strong>Celular:</strong> +57-3058172745<br>
+            <strong>Correo:</strong> hackmons5@gmail.com<br>
+          </p>
+        &copy; Copyright <strong>Helpme</strong>. Todos los derechos reservados
       </div>
     </div>
-  </footer><!-- #footer -->
+  </footer>
 
   <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
-  <!-- Uncomment below i you want to use a preloader -->
-  <!-- <div id="preloader"></div> -->
-
   <!-- JavaScript Libraries -->
   <script src="lib/jquery/jquery.min.js"></script>
   <script src="lib/jquery/jquery-migrate.min.js"></script>
